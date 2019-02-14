b729200365b20adb97f2afde26f1ee0fce1324a7
Jackson Forner <jef170030@utdallas.edu>
2019-02-13 23:42:49 -0600

    updated

--- HW 2/Precision.m
+++ HW 2/Precision.m
@@ -5,6 +5,17 @@
 % all x in the closed interval [0, pi/4]. Its function is to minimize
 % possible loss of significance incurred by the computer's computations.
 
+clc
+
 format long
 
-f(x) = sin(x) - 1 + cos(x)
+p = Precision(2);
+
+function f = Precision(x)
+    % Computes machine-precise values for the expression sin(x) - 1 + cos(x)
+    
+    f(x) = sin(x) - 2*(sin(x/2))^2; % this is the function obtained
+                                    % using a trigonometric identity and 
+                                    % rearranging accordingly
+end
+
