90f5774db56330c6ee5fc4423d3e70a79a8d9c75
jef170030 <jef170030@utdallas.edu>
2019-02-28 15:03:36 -0600

    yep

--- HW 6/3.2 Problems/first.m
+++ HW 6/3.2 Problems/first.m
@@ -6,5 +6,17 @@
 % its accompanied function value
 
 fx = @(x) tan(x) - x;
+fp = @(x) sec(x)^2 - 1; % the first derivative function for f
 gx = @(x) e^x - sqrt(x+9);
+gp = @(x) e^x - .5*(x+9)^(.5); % the first derivative function for g
 x0 = 2; % initial guess
+nmax =
+
+for n = 1 : nmax
+    if abs(fp) < delta
+        break
+        
+    
+    
+    
+    
