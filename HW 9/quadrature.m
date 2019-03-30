7f5aa5bb6103d5d5223f5848346fd0a43b546462
Jackson Forner <jef170030@utdallas.edu>
2019-03-30 17:15:29 -0500

    HW 9

---
+++ HW 9/quadrature.m
@@ -0,0 +1,16 @@
+% Section 001, Exercise 5.4.1
+
+% The function of this program is to provide a quadrature rule derived from a
+% general formula seen in the book (exercise 8). It uses appropriate
+% weights w and function evaluations of the integrand f and its first
+% derivative f' at the lower and upper bounds of integration a and b,
+% respectively. This is used to approximate the value of the integral of f
+% over the interval [a, b].
+
+function integral = quadrature(f, a, b)
+    % input f: integrand (function)
+    % input a: lower bound of integration
+    % input b: upper bound of integration
+        
+end
+
