0f49dd0f1acbd51c43b3b8f1f92c80743567e9fb
jef170030 <42558507+jef170030@users.noreply.github.com>
2019-04-03 23:07:10 -0500

    hw 9

--- HW 9/outputs.m
+++ HW 9/outputs.m
@@ -8,9 +8,25 @@
 % 2a. We are given the following function f1 with bounds a = 0 and b = 1:
 
 f1 = inline('1/sqrt(x)', 'x');
-answer1 = quadrature(f1, 0, 1)
+approximate1 = quadrature(f1, 0, 1)
+
+% Here is the actual value:
+
+syms x
+real1 = vpaintegral(1/sqrt(x), 0, 1)
+
+% Hence the error for 2a is:
+error1 = real1 - approximate1
 
 % 2b. We now approximate the integral of f2 over the interval [0, 2]:
 
 f2 = inline('exp(-cos(x)^2)', 'x');
-answer2 = quadrature(f2, 0, 2)
+approximate2 = quadrature(f2, 0, 2)
+
+% The vpa function reports 
+
+syms x
+real2 = vpaintegral(exp(-cos(x)^2), 0, 2)
+
+% Hence we have an error of
+error2 = real2 - approximate2
