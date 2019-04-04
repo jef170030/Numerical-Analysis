69fd8506b3027d8cb38849ec840489f2e406b17e
jef170030 <42558507+jef170030@users.noreply.github.com>
2019-04-03 22:45:56 -0500

    HW 9

--- HW 9/outputs.m
+++ HW 9/outputs.m
@@ -5,12 +5,12 @@
 % the function "quadrature". That is, we wish to approximate the integrals
 % of different functions f over different bounds of integration.
 
-% 2a. We are given the following function f1 with bounds a = 0 and b = 1.
+% 2a. We are given the following function f1 with bounds a = 0 and b = 1:
 
 f1 = inline('1/sqrt(x)', 'x');
 answer1 = quadrature(f1, 0, 1)
 
-% 2b. We now approximate the integral of f2 over the interval [0, 2].
+% 2b. We now approximate the integral of f2 over the interval [0, 2]:
 
 f2 = inline('exp(-cos(x)^2)', 'x');
 answer2 = quadrature(f2, 0, 2)
