8d3731d0db35177e608205ed7c2bed75c0828d50
jef170030 <42558507+jef170030@users.noreply.github.com>
2019-04-11 00:47:56 -0500

    hw 10

--- HW 10/Naive_Gauss_Random.m
+++ HW 10/Naive_Gauss_Random.m
@@ -1,4 +1,4 @@
-% Section 001, Exercise 2.1.3
+% Section 001, Exercise 2.1.8
 
 % The purpose of this function is to be able to solve an invertible system
 % of n linear equations in n unknowns by using the method of naive Gaussian
@@ -6,24 +6,22 @@
 % used to solve a given equation Ax = b.
 
 
-function x = Naive_Gauss(n)
+function x = Naive_Gauss_Random(n)
     % Input n: the number of rows or the number of columns of the square n
     % by n matrix A
-    % Input A: the matrix of coefficients
-    % Input b: the right hand side vector
      
-   A = zeros(n, n); % creating a n by n matrix which will be filled in
-   b = zeros(n, 1); % creating a n by 1 column vector, also to be filled in
-   x = zeros(n, 1); % the last vector we need filled in - the solution vector
-   
    % Defining the desired matrix A and vector b
    
+    A = rand(n);  % creating a random n by n matrix
+    j = 1 : n;
+    x = j'; % creating the desired solution vector x such that x(j) = j for j = 1 to n
+    b = zeros(n, 1); % creating the right hand side vector b (to be filled in)
+    
     for i = 1 : n
-        b(i) = i + 1;
-        for j = 1 : n
-            A(i, j) = i + j;
-        end
+        b(i) =  dot(A(i, :), x); % defining b to be our desired solution
     end
+    
+    x = zeros(n, 1);
     
     % Here are the steps for forward elimination:
     
