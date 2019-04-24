6970b49200fae006e19436ebed8e267c8c8a4f95
Jackson Forner <jef170030@utdallas.edu>
2019-04-24 16:56:17 -0500

    hw 11

--- HW 11/2.2 Problems/bigsystems.m
+++ HW 11/2.2 Problems/bigsystems.m
@@ -1,25 +1,18 @@
-%n = 5;
-%m = 4;
-%l = 1 : n;
-%s = zeros(n, 1);
+n = 5;
+m = 4;
+l = 1 : n;
+s = zeros(n, 1);
 
-%A = rand(n, n);
-%B = rand(n, m);
-%X = zeros(n, m);
+A = rand(n, n);
+B = rand(n, m);
+X = zeros(n, m);
 
-n = 2;
-m = 2;
-l = 1:n;
-
-A = [1 2; 4 2];
-B = eye(2);
-
+[Anew, l] = Gauss(n, A, l); 
 
 for i = 1 : m
     
     b = B(:, i);
-    [A, l] = Gauss(n, A, l); 
-    X(:,i) = Solve(n, A, l, b);
+    X(:,i) = Solve(n, Anew, l, b);
     
 end
 
